df625d918bbb2a9031f83e280d4ae613ad7899a8
Kevin Williams <kevin@k9w.org>
2022-01-12 16:21:35 -0800

    Fix person1 to .person1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
-  console.log("Script executing.");
   $("#formOne").submit(function() {
     event.preventDefault();
     const person1Input = $("input#person1").val();
-    console.log(person1Input);
     const person2Input = $("input#person2").val();
     const animalInput= $("input#animal").val();
     const exclamationInput = $("input#exclamation").val();
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
 
-    $("person1").text(person1Input);
+    $(".person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
